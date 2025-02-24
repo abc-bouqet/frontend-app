7c034b97561737ea420d529324a1a2dcd0625c68
shrivastav-aayush <aayush.shrivastav@digicert.com>
2025-02-24 19:01:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const fetchMessage = async () => {
+      try {
+          const response = await axios.get("http://localhost:4002/backend/hello");
+          console.log(response.data);
+          setMessage(response.data);
+      } catch (error) {
+          setMessage("Error fetching message");
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        
+          <h1>Landing Page</h1>
+          <button onClick={fetchMessage}>Get Backend Message</button>
+          <p>{message}</p>
+      </div>
   );
 }
 
